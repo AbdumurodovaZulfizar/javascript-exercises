329d93696d7053087cd8615dc30d39d080433f0b
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-08-19 14:44:31 +0500

    Add solution for tem Conversation

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,20 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+  
+  let value = ((num-32)*5/9);
+  if (value === parseInt(value)) {
+    return value;
+  }else {
+    return Number(value.toFixed(1));
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(num) {
+  let value = (num*9/5 + 32);
+  if (value=== parseInt(value)) {
+    return value;
+  } else {
+    return Number(value.toFixed(1));
+  }
 };
 
 module.exports = {
