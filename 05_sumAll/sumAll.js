58c39143889e4b319f42101c5768b07458a5b341
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-08-19 12:03:38 +0500

    Add solution for sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,15 @@
-const sumAll = function() {
+const sumAll = function(...values) {
+  let number_one = values.sort()[0];
+  let number_two = values.sort()[1];
+  if (number_two < 0 || number_one < 0 || number_two !== Number(number_two) || number_one !== Number(number_one)) {
+    return "ERROR";
+  }
+  let i = number_one, sum = 0;
+  while (i <= number_two) {
+    sum += i;
+    i+=1
+  }
+  return sum;
 
 };
 
