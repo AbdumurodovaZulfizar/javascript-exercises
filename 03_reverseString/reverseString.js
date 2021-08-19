b6f87be87d61d5e8b5e5a1e20c9a469dd61546ea
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-08-19 11:12:50 +0500

    Add solution for reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
 const reverseString = function(str) {
-
+  let i = str.length - 1;
+  let result = '';
+  while(i >= 0) {
+    result += str[i];
+    i-=1;
+  }
+  return result;
 };
 
 module.exports = reverseString;
