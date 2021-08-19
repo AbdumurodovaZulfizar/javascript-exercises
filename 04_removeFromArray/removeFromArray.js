4814c80d66eafddd5db19be6c08ff7455e326229
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-08-19 11:50:26 +0500

    Add solution for removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(...values) {
+  let arr = values[0], store = []
+  let i = 1;
+  while (i < values.length) {
+    store.push(values[i]);
+    i+=1;
+  }
 
+  for(let j = 0; j < store.length; j++) {
+    if (arr.indexOf(store[j]) >= 0) {
+    arr.splice(arr.indexOf(store[j]), 1);
+    }
+  }
+  return arr;
 };
 
 module.exports = removeFromArray;
