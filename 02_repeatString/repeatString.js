c5c10b45c0f16ec0069df441dd427e08f18a1a78
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-08-19 10:57:56 +0500

    Add solution for repeat string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+  let i = 0;
+  while (i < num) {
+    str+=str;
+    i+=1;
+  }
+  return str;
 };
 
 module.exports = repeatString;
