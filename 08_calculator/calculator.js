fa353babc7f7d34222e5f1c2d6ba79e0db7af7fa
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-08-25 17:26:36 +0500

    Create methods for calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
+const add = function(...values) {
+  let sum  = 0;
+  for(let i = 0; i< values.length; i++) {
+    sum += values[i];
+  }
+	return sum;
+};
+
+const subtract = function(...args) {
+  let first = args[0];
+  let second  = args[1];
+  return first - second;
 	
 };
 
-const subtract = function() {
-	
+const sum = function(array) {
+	if(array.length == 0) {
+    return 0;
+  } else {
+    return array.reduce((sum, ele) =>  sum + ele )
+  }
 };
 
-const sum = function() {
-	
+const multiply = function(array) {
+  if(array.length == 0) {
+    return 0;
+  } else {
+    return array.reduce((sum, ele) =>  sum * ele )
+  }
 };
 
-const multiply = function() {
-
+const power = function(number, times) {
+  let array = [];
+  for(let i = 0; i< times; i++) {
+    array.push(number)
+  }
+  return array.reduce((power, num) => power*num);
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  let array = [];
+	if (num <= 0) {
+    return 1;
+  } else {
+    for(let i = 1; i <= num; i++) {
+      array.push(i);
+    }
+    return array.reduce((fac, num) => fac*num)
+  }
 };
 
 module.exports = {
