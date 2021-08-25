6500a3fd7c6db55bc8a13dd5b097350219f2a7e2
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-08-25 17:46:54 +0500

    Add solution for fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if (num <=0 || Number(num) != num) {
+    return 'OOPS'
+  }
+  let fibonacciarray = [];
+  fibonacciarray[0]= 1;
+  fibonacciarray[1]= 1;
+  for(let i = 2; i < num; i++) {
+    fibonacciarray[i]= fibonacciarray[i-1]+ fibonacciarray[i-2]
+  }
+  return fibonacciarray[num-1];
 };
 
 module.exports = fibonacci;
