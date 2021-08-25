2e055b925d5a104a28fee825d3641dc045e99c61
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-08-25 17:38:33 +0500

    Add solutions for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let newStr = str.trim().replace(/[^0-9a-z]/gi, '');
+  let final  =newStr.toLowerCase();
+  let result = final.split("").reverse().join("");
+  return result === final;
 };
 
 module.exports = palindromes;
