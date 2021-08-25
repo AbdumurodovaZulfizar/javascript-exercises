464f6d126635a3cd634c85700e04672ce9e0f108
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-08-25 18:28:10 +0500

    Add solution for oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+  let myarray = [];
+    for(let i = 0 ; i < array.length; i++) {
+      if ('yearOfDeath' in array[i]) {
+      myarray.push(array[i]);} else {
+        array[i].yearOfDeath = new Date().getFullYear();
+        myarray.push(array[i]);
+      }
+    }
+  let final = myarray.sort((a, b) => (a.yearOfDeath - a.yearOfBirth)> (b.yearOfDeath - b.yearOfBirth) ? -1:1);
+  return final[0];
 };
 
 module.exports = findTheOldest;
